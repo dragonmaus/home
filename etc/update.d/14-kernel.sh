3a8e0a320dd48b6d2d4e5515ff54802fcb5a56ca
David Wildasin <dragonmaus@posteo.net>
2018-06-02 09:07:40 -0400

    Make kernel check more verbose

--- etc/update.d/14-kernel.sh
+++ etc/update.d/14-kernel.sh
@@ -1,7 +1,19 @@
 l=`pacaur -Qi linux | sed -n 's/^Version \+: //p'`
 r=`pacaur -Si linux | sed -n 's/^Version \+: //p'`
 c=`vercmp "$l" "$r"`
-if test x"$c" = x-1; then
-  echo "Linux kernel is outdated, please update!" 1>&2
-  exit 0
-fi
+echo "Checking kernel version…" 1>&2
+case $c in
+  -1)
+    echo "$l (local) < $r (remote)" 1>&2
+    echo "Linux kernel is outdated, please update!" 1>&2
+    exit 0
+    ;;
+  0)
+    echo "$l (local) = $r (remote)" 1>&2
+    echo "Linux kernel is up-to-date!" 1>&2
+    ;;
+  1)
+    echo "$l (local) > $r (remote)" 1>&2
+    echo "Future kernel detected!" 1>&2
+    ;;
+esac
