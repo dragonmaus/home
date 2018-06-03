5c53c3bce09187b2e3eac0c591ac1def98fbbec2
David Wildasin <dragonmaus@posteo.net>
2018-06-03 17:49:08 -0400

    Clean up output a bit

--- etc/update.d/14-kernel.sh
+++ etc/update.d/14-kernel.sh
@@ -1,19 +1,22 @@
 l=`pacaur -Qi linux | sed -n 's/^Version \+: //p'`
 r=`pacaur -Si linux | sed -n 's/^Version \+: //p'`
 c=`vercmp "$l" "$r"`
+vertab() {
+  printf '%s\t%c\t%s\n' local ' ' remote "$1" "$2" "$3" | column -s '	' -t
+}
 echo "Checking kernel version…" 1>&2
 case $c in
   -1)
-    echo "$l (local) < $r (remote)" 1>&2
+    vertab $l '<' $r 1>&2
     echo "Linux kernel is outdated, please update!" 1>&2
     exit 0
     ;;
   0)
-    echo "$l (local) = $r (remote)" 1>&2
+    vertab $l '=' $r 1>&2
     echo "Linux kernel is up-to-date!" 1>&2
     ;;
   1)
-    echo "$l (local) > $r (remote)" 1>&2
+    vertab $l '>' $r 1>&2
     echo "Future kernel detected!" 1>&2
     ;;
 esac
