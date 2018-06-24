8727043d9631f07b2d52452a3f16258d77c8f3ab
David Wildasin <dragonmaus@posteo.net>
2018-06-24 11:42:55 -0400

    haskell: Reorganise personal library

--- src/haskell/my/src/My/List.hs
+++ src/haskell/my/src/My/List.hs
@@ -1,7 +1,16 @@
 module My.List where
 
-import Data.List
+import Data.List ( filter, groupBy )
+
+contains :: Eq 𝑎 => [𝑎] -> 𝑎 -> Bool
+contains    []  𝑦             = False
+contains (𝑥:𝑥𝑠) 𝑦 | 𝑥 == 𝑦    = True
+                  | otherwise = 𝑥𝑠 `contains` 𝑦
 
 prune :: Eq α => α -> [α] -> [α]
-prune _ [] = []
-prune 𝑥 (𝑦:𝑦𝑠) = if 𝑥 == 𝑦 then 𝑦𝑠 else 𝑦 : 𝑦𝑠
+prune _    []              = []
+prune 𝑥 (𝑦:𝑦𝑠) | 𝑥 == 𝑦    = 𝑦𝑠
+               | otherwise = 𝑦 : 𝑦𝑠
+
+splitBy :: Eq 𝑎 => 𝑎 -> [𝑎] -> [[𝑎]]
+splitBy 𝑐 = filter (/= [𝑐]) . groupBy (\𝑥 𝑦 -> 𝑥 /= 𝑐 && 𝑦 /= 𝑐)
--- src/haskell/my/src/My/String.hs
+++ src/haskell/my/src/My/String.hs
@@ -1,11 +1,6 @@
 module My.String where
 
-import Data.List
-
 pad :: Int -> Char -> String -> String
 pad 𝑙 𝑐 𝑠 = if length 𝑠 < 𝑙
             then 𝑐 : pad (𝑙 - 1) 𝑐 𝑠
             else 𝑠
-
-splitBy :: Char -> String -> [String]
-splitBy 𝑐 = filter (/= [𝑐]) . groupBy (\𝑥 𝑦 -> 𝑥 /= 𝑐 && 𝑦 /= 𝑐)
