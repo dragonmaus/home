1cb9392d93bfd7f35f997701819dfe3eda7c6a70
David Wildasin <dragonmaus@posteo.net>
2018-07-14 22:49:57 -0400

    bin: cabal reset script improvements

--- src/bin/cabal-system-reset/cabal-system-reset.sh
+++ src/bin/cabal-system-reset/cabal-system-reset.sh
@@ -8,13 +8,15 @@ cabal=$HOME/.cabal
 ghc=$HOME/.ghc
 hackage=$HOME/.hackage
 
-rm -fr $cabal $ghc $hackage
+rm -fr $cabal $ghc
+rm -f $hackage/repo/*
+rm -fr $hackage/repo/index
 git diff -R --binary -- $cabal $ghc $hackage | git apply
 
-src=$HOME/src/haskell
+src=$HOME/src/haskell/src
 
 mkdir -pv $hackage/repo/package
-ln -v $src/*/dist/*.tar.gz $hackage/repo/package
+ln -fv $src/*/dist/*.tar.gz $hackage/repo/package
 
 hackage-repo-tool create-keys --keys $hackage/keys
 hackage-repo-tool bootstrap --keys $hackage/keys --repo $hackage/repo
@@ -24,11 +26,12 @@ mkdir -pv $cabal/packages/local
 
 cabal update
 
-for package in `cat $cabal.world`; do
+packages=`cat $cabal.world`
+for package in $packages; do
   case $package in /*) ;; *) continue ;; esac
   test -d $package || continue
   (cd $package && exec cabal clean)
 done
 
 cabal install hscolour # must be installed first
-cabal install `cat $cabal.world`
+cabal install $packages
